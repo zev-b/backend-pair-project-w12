982db5201d8f82fcf12790da41ff12e7b73789d5
zev-b <zevbistritz@gmail.com>
2024-09-18 17:03:13 -0400

    Test fetch req in Dev tools to server possible problem

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,10 @@
 // backend/routes/index.js
 const express = require('express');
-const router = express.Router();
+const router = express.Router(); 
+
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
 
 // Add a XSRF-TOKEN cookie
 router.get("/api/csrf/restore", (req, res) => {
