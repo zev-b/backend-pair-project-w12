250f34f6b4d55ab90672f8176c3780ebf38e4d87
zev-b <zevbistritz@gmail.com>
2024-09-18 21:17:06 -0400

    Added User logout backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -44,6 +44,16 @@ router.post(
         user: safeUser
       });
     }
-);
+); 
+
+
+// Log out
+router.delete(
+    '/',
+    (_req, res) => {
+      res.clearCookie('token');
+      return res.json({ message: 'success' });
+    }
+  );
 
 module.exports = router;
