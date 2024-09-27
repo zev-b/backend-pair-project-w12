31cc9b4bf57646ae8ee8535f8d4ed3df359cfa43
zev-b <zevbistritz@gmail.com>
2024-09-27 17:47:50 -0400

    "Merging Debacle"

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -2,8 +2,8 @@
 const router = require('express').Router();
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
-
-
+const reviewsRouter = require('./reviews.js');
+const bookingsRouter = require('./bookings.js');
 const spotsRouter = require('./spots.js')
 const { restoreUser } = require("../../utils/auth.js");
 
@@ -16,7 +16,11 @@ router.use('/session', sessionRouter);
 
 router.use('/users', usersRouter); 
 
-router.use('/spots', spotsRouter);
+router.use('/spots', spotsRouter); 
+
+router.use('/reviews', reviewsRouter);
+
+router.use('/bookings', bookingsRouter);
 
 router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
