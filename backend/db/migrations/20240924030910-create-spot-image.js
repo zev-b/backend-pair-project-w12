fbc69e8b8322fe071c560c66573d6c022e167d39
zev-b <zevbistritz@gmail.com>
2024-09-27 17:51:02 -0400

    "merging"

--- backend/db/migrations/20240924030910-create-spot-image.js
+++ backend/db/migrations/20240924030910-create-spot-image.js
@@ -4,11 +4,8 @@ let options = {};
 if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;  // define your schema in options object
 }
-<<<<<<< HEAD
- 
-=======
 
->>>>>>> reviewsRoutes
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
